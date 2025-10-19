2f7539f22a95ef8bb0e4886cf466380a1ec9aca2
omeryildiz <OmerFarukYildiz2004@outlook.com>
2025-10-19 22:54:53 +0300

    feat: add @ComponentScan to starter package

--- src/main/java/com/omeryildiz/starter/SpringRestApiApplication.java
+++ src/main/java/com/omeryildiz/starter/SpringRestApiApplication.java
@@ -2,7 +2,8 @@ package com.omeryildiz.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
+@ComponentScan(basePackages = {"com.omeryildiz"})
 @SpringBootApplication
 public class SpringRestApiApplication {
 
